7d39e125225a8bb81de132723b6b91abc88b90d5
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-09-24 00:30:01 -0500

    play with .find and findIndex

--- main.js
+++ main.js
@@ -362,3 +362,12 @@ console.log(new Date());
 //   .get( function () {
 //     return dateFormat( this.created )
 //   } );
+
+
+var array1 = [5, 12, 8, 130, 44];
+
+var found = array1.find(function(element) {
+  return element > 10;
+});
+
+console.log(found);
